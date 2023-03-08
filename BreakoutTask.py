7ffa4f7fb2563a7c93a59287fa84803cf0dd0e95
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-03-08 09:23:21 +0000

    Create BreakoutTask.py

---
+++ BreakoutTask.py
@@ -0,0 +1,31 @@
+# code recipes here: https://pyowm.readthedocs.io/en/latest/v3/code-recipes.html#onecall
+# https://pyowm.readthedocs.io/_/downloads/en/develop/pdf/
+
+from pyowm.owm import OWM  #importing the open weather map python library
+
+myOwm = OWM('***API key goes here***')  #using your API key to tell OWM who you are
+
+mgr = myOwm.weather_manager()   # command to access the weather using your API
+
+
+# _________________________________________________________________
+# |                           Task                                |
+# |                   --------------------                        |
+# | Given the locations below, find the optimal location in which |
+# | to build a new offshore wind farm.                            |
+# | Use data from the OWM API to determine this location          |
+# | and justify your decision.                                    |
+# |_______________________________________________________________|
+
+
+locations = ['Killybegs, IE', 'Belmullet, IE', 'Clifden, IE', 'Doolin, IE', 'Kilkee, IE', 'Ventry, IE','Portmagee, IE','Bantry, IE','Baltimore, IE']
+
+
+
+
+
+
+
+
+#answer = 
+#print("the ideal location for a new windfarm is",answer)
